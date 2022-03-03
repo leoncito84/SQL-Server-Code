1b35da40bc1b9d459988e386ad5af9e28a342363
Leon Armstrong <leoncito@live.com>
2022-03-03 00:28:31 +0000

    SQL Basics Create Table and Insert Into
    Create SQL Basics Create Table and Insert Into

---
+++ SELECT_EmployeeSystems.sql
@@ -0,0 +1,13 @@
+/*
+Select Statement
+*, Top, Distinct, Count, As, Max, Min, Av
+*/
+
+--SELECT TOP 6*
+--SELECT MAX (Salary)
+-- SELECT MIN (Salary)
+--SELECT COUNT (LastName) AS LastNameCount
+--SELECT AVG(Salary)  
+SELECT *
+FROM EmployeeSystems
+--FROM EmployeeSalary
